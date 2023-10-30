05a9aa6c4d4688f42014960ac99d1bfe404cf49b
Natanael Martinez Ramirez <86125474+2000Nathan@users.noreply.github.com>
2023-10-30 16:40:44 -0600

    Update connection.ts
    update db

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize  } from "sequelize";
 
 //Conexión a la base de datos nombre de la base de datos, usuario, contraseña
-const sequelize = new Sequelize('dbmicolchonsito', 'root', '2023Natan,.-', {
+const sequelize = new Sequelize('******', '******', '*******', {
     host: 'localhost',
     dialect: 'mysql'
 });
